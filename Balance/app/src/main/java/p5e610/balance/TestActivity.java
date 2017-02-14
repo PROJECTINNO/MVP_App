72f5dc3eda4e03aebcfb4ae1f66e35c9b6d561b7
Mathias Loh <kyd1992@gmail.com>
2017-02-14 11:07:48 +0100

    Fixed most things!

--- Balance/app/src/main/java/p5e610/balance/TestActivity.java
+++ Balance/app/src/main/java/p5e610/balance/TestActivity.java
@@ -25,118 +25,117 @@ import java.util.ArrayList;
  */
 
 public class TestActivity extends AppCompatActivity {
-
-    private double duration;
-    private ArrayList<Double> accx;
-    private ArrayList<Double> accy;
-    private ArrayList<Double> accz;
-    private SensorManager sensorManager;
-    private boolean started = false;
-    private AccelerationData sensorData;
-    private AudioManager audioManager;
-    float[] mGravf = null;
-    float[] mMagnetf = null;
-
-    private static final int REQUEST_EXTERNAL_STORAGE = 1;
-    private static String[] PERMISSIONS_STORAGE = {
-            Manifest.permission.READ_EXTERNAL_STORAGE,
-            Manifest.permission.WRITE_EXTERNAL_STORAGE
-    };
+//
+//    private double duration;
+//    private ArrayList<Double> accx;
+//    private ArrayList<Double> accy;
+//    private ArrayList<Double> accz;
+//    private SensorManager sensorManager;
+//    private boolean started = false;
+//    private AccelerationData sensorData;
+//    private AudioManager audioManager;
+//    float[] mGravf = null;
+//    float[] mMagnetf = null;
+//
+//    private static final int REQUEST_EXTERNAL_STORAGE = 1;
+//    private static String[] PERMISSIONS_STORAGE = {
+//            Manifest.permission.READ_EXTERNAL_STORAGE,
+//            Manifest.permission.WRITE_EXTERNAL_STORAGE
+//    };
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        NotificationManager notificationManager = (NotificationManager) this.getSystemService(this.NOTIFICATION_SERVICE);
-        layout = (RelativeLayout) findViewById(R.id.chart_container);
-        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-        sensorData = new AccelerationData();
-        verifyStoragePermissions(this);
-        accx = new ArrayList<Double>();
-        accy = new ArrayList<Double>();
-        accz = new ArrayList<Double>();
-
-        audioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
-        audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, 20, 0);
-
-        mPrepField = (EditText) findViewById((R.id.prepText));
-        mTextField = (EditText) findViewById(R.id.editText);
-        mTextField.setEnabled(false);
-        btnStop = (Button) findViewById(R.id.btnStop);
-        btnStart = (Button) findViewById(R.id.btnStart);
-        btnAcceleration = (Button) findViewById(R.id.btnAcceleration);
-        btnUpload = (Button) findViewById(R.id.btnUpload);
-        btnStart.setOnClickListener(this);
-        btnAcceleration.setOnClickListener(this);
-        btnUpload.setOnClickListener(this);
-        btnStop.setOnClickListener(this);
-        btnStart.setEnabled(true);
-        btnAcceleration.setEnabled(false);
-        if (sensorData == null || sensorData.getX().size() == 0) {
-            btnUpload.setEnabled(false);
-        }
-    }
-
-    //Display notification
-
-    public static void verifyStoragePermissions(Activity activity) {
-        // Check if we have read or write permission
-        int writePermission = ActivityCompat.checkSelfPermission(activity, Manifest.permission.WRITE_EXTERNAL_STORAGE);
-        int readPermission = ActivityCompat.checkSelfPermission(activity, Manifest.permission.READ_EXTERNAL_STORAGE);
-
-        if (writePermission != PackageManager.PERMISSION_GRANTED || readPermission != PackageManager.PERMISSION_GRANTED) {
-            // We don't have permission so prompt the user
-            ActivityCompat.requestPermissions(
-                    activity,
-                    PERMISSIONS_STORAGE,
-                    REQUEST_EXTERNAL_STORAGE
-            );
-        }
-    }
-
-    @Override
-    public void onSensorChanged(SensorEvent event) {
-
-        // Gets the value of the sensor that has been changed
-        if (started) {
-            if ((mGravf != null) && (mMagnetf != null) && (event.sensor.getType() == Sensor.TYPE_LINEAR_ACCELERATION)) {
-//            if (event.sensor.getType()==Sensor.TYPE_LINEAR_ACCELERATION){
-                long timestamp = System.currentTimeMillis();
-                float[] deviceRelativeAcceleration = new float[4];
-                deviceRelativeAcceleration[0] = event.values[0];
-                deviceRelativeAcceleration[1] = event.values[1];
-                deviceRelativeAcceleration[2] = event.values[2];
-                deviceRelativeAcceleration[3] = 0;
-
-
-                // ----- If we want relative acceleration -------- //
-//                sensorData.addX(deviceRelativeAcceleration[0]);
-//                sensorData.addY(deviceRelativeAcceleration[1]);
-//                sensorData.addZ(deviceRelativeAcceleration[2]);
+//        NotificationManager notificationManager = (NotificationManager) this.getSystemService(this.NOTIFICATION_SERVICE);
+//        layout = (RelativeLayout) findViewById(R.id.chart_container);
+//        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+//        sensorData = new AccelerationData();
+//        verifyStoragePermissions(this);
+//        accx = new ArrayList<Double>();
+//        accy = new ArrayList<Double>();
+//        accz = new ArrayList<Double>();
+//
+//        audioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+//        audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, 20, 0);
+//
+//        mPrepField = (EditText) findViewById((R.id.prepText));
+//        mTextField = (EditText) findViewById(R.id.editText);
+//        mTextField.setEnabled(false);
+//        btnStop = (Button) findViewById(R.id.btnStop);
+//        btnStart = (Button) findViewById(R.id.btnStart);
+//        btnAcceleration = (Button) findViewById(R.id.btnAcceleration);
+//        btnUpload = (Button) findViewById(R.id.btnUpload);
+//        btnStart.setOnClickListener(this);
+//        btnAcceleration.setOnClickListener(this);
+//        btnUpload.setOnClickListener(this);
+//        btnStop.setOnClickListener(this);
+//        btnStart.setEnabled(true);
+//        btnAcceleration.setEnabled(false);
+//        if (sensorData == null || sensorData.getX().size() == 0) {
+//            btnUpload.setEnabled(false);
+//        }
+//    }
+//
+//    //Display notification
+//
+//    public static void verifyStoragePermissions(Activity activity) {
+//        // Check if we have read or write permission
+//        int writePermission = ActivityCompat.checkSelfPermission(activity, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+//        int readPermission = ActivityCompat.checkSelfPermission(activity, Manifest.permission.READ_EXTERNAL_STORAGE);
+//
+//        if (writePermission != PackageManager.PERMISSION_GRANTED || readPermission != PackageManager.PERMISSION_GRANTED) {
+//            // We don't have permission so prompt the user
+//            ActivityCompat.requestPermissions(
+//                    activity,
+//                    PERMISSIONS_STORAGE,
+//                    REQUEST_EXTERNAL_STORAGE
+//            );
+//        }
+//    }
+//
+//    @Override
+//    public void onSensorChanged(SensorEvent event) {
+//
+//        // Gets the value of the sensor that has been changed
+//        if (started) {
+//            if ((mGravf != null) && (mMagnetf != null) && (event.sensor.getType() == Sensor.TYPE_LINEAR_ACCELERATION)) {
+////            if (event.sensor.getType()==Sensor.TYPE_LINEAR_ACCELERATION){
+//                long timestamp = System.currentTimeMillis();
+//                float[] deviceRelativeAcceleration = new float[4];
+//                deviceRelativeAcceleration[0] = event.values[0];
+//                deviceRelativeAcceleration[1] = event.values[1];
+//                deviceRelativeAcceleration[2] = event.values[2];
+//                deviceRelativeAcceleration[3] = 0;
+//
+//
+//                // ----- If we want relative acceleration -------- //
+////                sensorData.addX(deviceRelativeAcceleration[0]);
+////                sensorData.addY(deviceRelativeAcceleration[1]);
+////                sensorData.addZ(deviceRelativeAcceleration[2]);
+////                sensorData.addTimestamp(timestamp);
+//                // ----- If we want relative acceleration -------- //
+//
+//                // ----- If we want absolute acceleration -------- //
+//                float[] R = new float[16], I = new float[16], earthAcc = new float[16];
+//                SensorManager.getRotationMatrix(R, I, mGravf, mMagnetf);
+//                float[] inv = new float[16];
+//                android.opengl.Matrix.invertM(inv, 0, R, 0);
+//                android.opengl.Matrix.multiplyMV(earthAcc, 0, inv, 0, deviceRelativeAcceleration, 0);
+//                sensorData.addX(earthAcc[0]);
+//                sensorData.addY(earthAcc[1]);
+//                sensorData.addZ(earthAcc[2]);
 //                sensorData.addTimestamp(timestamp);
-                // ----- If we want relative acceleration -------- //
+//                // ----- If we want absolute acceleration -------- //
+//            }
+//            else if (event.sensor.getType() == Sensor.TYPE_GRAVITY){
+//                mGravf = event.values;
+//            } else if (event.sensor.getType() == Sensor.TYPE_MAGNETIC_FIELD){
+//                mMagnetf = event.values;
+//            }
+//        }
+//    }
 
-                // ----- If we want absolute acceleration -------- //
-                float[] R = new float[16], I = new float[16], earthAcc = new float[16];
-                SensorManager.getRotationMatrix(R, I, mGravf, mMagnetf);
-                float[] inv = new float[16];
-                android.opengl.Matrix.invertM(inv, 0, R, 0);
-                android.opengl.Matrix.multiplyMV(earthAcc, 0, inv, 0, deviceRelativeAcceleration, 0);
-                sensorData.addX(earthAcc[0]);
-                sensorData.addY(earthAcc[1]);
-                sensorData.addZ(earthAcc[2]);
-                sensorData.addTimestamp(timestamp);
-                // ----- If we want absolute acceleration -------- //
-            }
-            else if (event.sensor.getType() == Sensor.TYPE_GRAVITY){
-                mGravf = event.values;
-            } else if (event.sensor.getType() == Sensor.TYPE_MAGNETIC_FIELD){
-                mMagnetf = event.values;
-            }
-        }
-    }
-
-
-}
+    }}
 
 
