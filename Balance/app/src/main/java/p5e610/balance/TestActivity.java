d4e49271086452e23d4629339079beeccb37ee3e
Mathias Loh <kyd1992@gmail.com>
2017-02-14 10:19:04 +0100

    blocked out test activity for the moment. TODO: redesign the xml files

--- Balance/app/src/main/java/p5e610/balance/TestActivity.java
+++ Balance/app/src/main/java/p5e610/balance/TestActivity.java
@@ -23,74 +23,74 @@ import java.util.ArrayList;
 
 public class TestActivity extends AppCompatActivity {
 
-    private double duration;
-    private ArrayList<Double> accx;
-    private ArrayList<Double> accy;
-    private ArrayList<Double> accz;
-    private SensorManager sensorManager;
-    private boolean started = false;
-    private AccelerationData sensorData;
-    private AudioManager audioManager;
-
-    private static final int REQUEST_EXTERNAL_STORAGE = 1;
-    private static String[] PERMISSIONS_STORAGE = {
-            Manifest.permission.READ_EXTERNAL_STORAGE,
-            Manifest.permission.WRITE_EXTERNAL_STORAGE
-    };
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        NotificationManager notificationManager = (NotificationManager) this.getSystemService(this.NOTIFICATION_SERVICE);
-        layout = (RelativeLayout) findViewById(R.id.chart_container);
-        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-        sensorData = new AccelerationData();
-        verifyStoragePermissions(this);
-        accx = new ArrayList<Double>();
-        accy = new ArrayList<Double>();
-        accz = new ArrayList<Double>();
-
-        audioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
-        audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, 20, 0);
-
-        mPrepField = (EditText) findViewById((R.id.prepText));
-        mTextField = (EditText) findViewById(R.id.editText);
-        mTextField.setEnabled(false);
-        btnStop = (Button) findViewById(R.id.btnStop);
-        btnStart = (Button) findViewById(R.id.btnStart);
-        btnAcceleration = (Button) findViewById(R.id.btnAcceleration);
-        btnUpload = (Button) findViewById(R.id.btnUpload);
-        btnStart.setOnClickListener(this);
-        btnAcceleration.setOnClickListener(this);
-        btnUpload.setOnClickListener(this);
-        btnStop.setOnClickListener(this);
-        btnStart.setEnabled(true);
-        btnAcceleration.setEnabled(false);
-        if (sensorData == null || sensorData.getX().size() == 0) {
-            btnUpload.setEnabled(false);
-        }
-    }
-
-    //Display notification
-
-    public static void verifyStoragePermissions(Activity activity) {
-        // Check if we have read or write permission
-        int writePermission = ActivityCompat.checkSelfPermission(activity, Manifest.permission.WRITE_EXTERNAL_STORAGE);
-        int readPermission = ActivityCompat.checkSelfPermission(activity, Manifest.permission.READ_EXTERNAL_STORAGE);
-
-        if (writePermission != PackageManager.PERMISSION_GRANTED || readPermission != PackageManager.PERMISSION_GRANTED) {
-            // We don't have permission so prompt the user
-            ActivityCompat.requestPermissions(
-                    activity,
-                    PERMISSIONS_STORAGE,
-                    REQUEST_EXTERNAL_STORAGE
-            );
-        }
-    }
-
-
-}
-
+//    private double duration;
+//    private ArrayList<Double> accx;
+//    private ArrayList<Double> accy;
+//    private ArrayList<Double> accz;
+//    private SensorManager sensorManager;
+//    private boolean started = false;
+//    private AccelerationData sensorData;
+//    private AudioManager audioManager;
+//
+//    private static final int REQUEST_EXTERNAL_STORAGE = 1;
+//    private static String[] PERMISSIONS_STORAGE = {
+//            Manifest.permission.READ_EXTERNAL_STORAGE,
+//            Manifest.permission.WRITE_EXTERNAL_STORAGE
+//    };
+//
+//    @Override
+//    public void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//        NotificationManager notificationManager = (NotificationManager) this.getSystemService(this.NOTIFICATION_SERVICE);
+//        layout = (RelativeLayout) findViewById(R.id.chart_container);
+//        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+//        sensorData = new AccelerationData();
+//        verifyStoragePermissions(this);
+//        accx = new ArrayList<Double>();
+//        accy = new ArrayList<Double>();
+//        accz = new ArrayList<Double>();
+//
+//        audioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+//        audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, 20, 0);
+//
+//        mPrepField = (EditText) findViewById((R.id.prepText));
+//        mTextField = (EditText) findViewById(R.id.editText);
+//        mTextField.setEnabled(false);
+//        btnStop = (Button) findViewById(R.id.btnStop);
+//        btnStart = (Button) findViewById(R.id.btnStart);
+//        btnAcceleration = (Button) findViewById(R.id.btnAcceleration);
+//        btnUpload = (Button) findViewById(R.id.btnUpload);
+//        btnStart.setOnClickListener(this);
+//        btnAcceleration.setOnClickListener(this);
+//        btnUpload.setOnClickListener(this);
+//        btnStop.setOnClickListener(this);
+//        btnStart.setEnabled(true);
+//        btnAcceleration.setEnabled(false);
+//        if (sensorData == null || sensorData.getX().size() == 0) {
+//            btnUpload.setEnabled(false);
+//        }
+//    }
+//
+//    //Display notification
+//
+//    public static void verifyStoragePermissions(Activity activity) {
+//        // Check if we have read or write permission
+//        int writePermission = ActivityCompat.checkSelfPermission(activity, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+//        int readPermission = ActivityCompat.checkSelfPermission(activity, Manifest.permission.READ_EXTERNAL_STORAGE);
+//
+//        if (writePermission != PackageManager.PERMISSION_GRANTED || readPermission != PackageManager.PERMISSION_GRANTED) {
+//            // We don't have permission so prompt the user
+//            ActivityCompat.requestPermissions(
+//                    activity,
+//                    PERMISSIONS_STORAGE,
+//                    REQUEST_EXTERNAL_STORAGE
+//            );
+//        }
+//    }
+//
+//
+//}
+//
 
 }
