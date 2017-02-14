dc213cadb77e2a48efaf59e429b37f848733a108
JULIEN-PC\Julien <julien.bruno@student.ecp.fr>
2017-02-14 11:11:37 +0100

    junit accelerationdata

---
+++ Balance/app/src/test/java/p5e610/balance/AccelerationData.java
@@ -0,0 +1,20 @@
+package p5e610.balance;
+
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by Julien on 14/02/2017.
+ */
+
+public class AccelerationData {
+    @Test
+    public void add_is_correct() throws Exception {
+        ArrayList<Double> x = new ArrayList<Double>();
+        assertEquals(x.addX(1.0),x.add(1.0));
+    }
+}
