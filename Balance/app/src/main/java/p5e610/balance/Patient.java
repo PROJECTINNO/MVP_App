6f87e4a0d1544f962efd35879f832e5eff0f4384
Katelyn Weingart <katherine_weingart@brown.edu>
2017-03-28 16:10:07 +0200

    add  heroku database code, change patient/doctor parameters

--- Balance/app/src/main/java/p5e610/balance/Patient.java
+++ Balance/app/src/main/java/p5e610/balance/Patient.java
@@ -7,15 +7,9 @@ public class Patient extends User {
     private Doctor doctor;
     private ArrayList <AccelerationData> history;
 
-    public Patient(String name, String surname, String username, String email, Doctor doctor, ArrayList<AccelerationData> historique) {
+    public Patient(String name, String surname, String username, String email) {
         super(name, surname, username, email);
-        this.doctor = doctor;
-        this.history = historique;
     }
-
-    public ArrayList<AccelerationData> getHistory() {return history;}
-
-    public void setHistory(ArrayList<AccelerationData> history) {this.history = history;}
 
     public Doctor getDoctor() {return doctor;}
 
